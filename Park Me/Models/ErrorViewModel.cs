d71ddaeaf2ca915a89edec5f05df0444db48737a
unknown <safii.elmehdi@gmail.com>
2022-05-28 16:24:54 +0100

    lwla gae

---
+++ Park Me/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Park_Me.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Park Me/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(".card-button").click(function () {
+    $(".modalcontainer,.modal").fadeIn("slow");
+});
+
+$(".close,.buttons").click(function () {
+    $(".modalcontainer,.modal").fadeOut("slow");
+});
+
+$(document).ready(function () {
+    $('.select2').select2({
+        closeOnSelect: false
+    });
+});
